4ebb04bde90b3bb98f16c8f63ccb8879d1f7e6d0
sherry <zsherry0803@gmail.com>
2022-05-07 20:51:26 -0400

    controller done without 400 error

--- src/main/java/com/nr/stringville/StringvilleController.java
+++ src/main/java/com/nr/stringville/StringvilleController.java
@@ -1,5 +1,10 @@
 package com.nr.stringville;
 
+import com.nr.stringville.dto.User;
+import com.nr.stringville.dto.UserDTO;
+import com.nr.stringville.service.Service;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,28 +15,48 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class StringvilleController {
 
+    @Autowired
+    private ModelMapper modelMapper;
+
+    private Service service;
+
+    public StringvilleController(Service service) {
+        super();
+        this.service = service;
+    }
+
     @RequestMapping("/")
     public String index() {
         return "Welcome to Stringville!";
     }
 
     @PostMapping("/submission")
-    public ResponseEntity submission(@RequestBody String body) {
+    public ResponseEntity submission(@RequestBody String submission) {
+        String[] submissionArray = submission.split(",",1);
+        UserDTO userDTO = new UserDTO();
+        String str = submissionArray[0];
+        userDTO.setName(submissionArray[1]);
+        User userSubmission = modelMapper.map(userDTO, User.class);
+
+        User user = service.updateUser(userSubmission.getName(), str);
         return ResponseEntity.status(200).body("accepted");
     }
 
     @GetMapping("/results")
     public ResponseEntity results() {
-        return ResponseEntity.status(200).body("");
+        String res = service.getResult();
+        return ResponseEntity.status(200).body(res);
     }
 
     @GetMapping("/health")
     public ResponseEntity health() {
-        return ResponseEntity.status(200).body("");
+        String res = service.getHealth();
+        return ResponseEntity.status(200).body(res);
     }
 
     @GetMapping("/reset")
     public ResponseEntity reset() {
+        service.reset();
         return ResponseEntity.status(200).body("");
     }
 
